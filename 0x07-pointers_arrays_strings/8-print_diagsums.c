9bae2ce2db34be93ed0b34c22082b08e6b2e05bd
new-coming <yassine.semlali77@gmail.com>
2023-04-04 09:21:06 -0700

    t1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,42 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_diagsums - prints the sum of the two diaginal of a square
- * matrix of integers
- *
- * @a: pointer to matrix of integers
- * @size: size of matrix
- *
+ * print_diagsums - Entry point
+ * @s: array
+ * @size: size of array
+ * Return: Always 0 (Success)
  */
-void print_diagsums(int *a, int size)
-{
-	int sum1, sum2, x, y, z;
 
-	y = 0; z = size - 1;
-	sum1 = sum2 = 0;
-	for (x = 0; x < size; x++, y += size + 1, z += size - 1)
+void print_diagsums(int *s, int size)
+{
+	int i;
+	int j;
+	int sum_diag1;
+	int sum_diag2;
+
+	sum_diag1 = 0;
+	sum_diag2 = 0;
+
+	/* The first Diagonal from left */
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + y);
-		sum2 += *(a + z);
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum_diag1 = sum_diag1 + *(s + (i * size) + j);
+			}
+		}
 	}
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		for (j = (size - 1); j >= 0; j--)
+		{
+			if (j == (size - 1 - i))
+			{
+				sum_diag2 = sum_diag2 + *(s + (i * size) + j);
+			}
+		}
+	}
+	printf("%d, %d\n", sum_diag1, sum_diag2);
 }
